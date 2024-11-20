860282fdf86f2d81ace2dfb35d3656af5af7eab9
Alexandre Rabald <alerabald@etmlnet.local>
2024-11-20 15:10:02 +0100

    correction Q3 Q6

---
+++ ARD/Q6/Test1_JJE/Source.c
@@ -0,0 +1,21 @@
+//-- librairies standard --//
+#include <stdint.h>
+#include "testQ4.h"
+
+//-- variable globales --//
+
+
+//-- programme principal --//
+void main()
+{
+	int frame = 0;
+	char Tension = 0;
+	char Courant = 0;
+	char Puissance = 0;
+	char Résistance = 0;
+
+	frame = frame + Tension;
+	frame = frame + Courant;
+	frame = frame + Puissance;
+	frame = frame + Résistance;
+}
